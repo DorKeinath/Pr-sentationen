207396513948e6d0e4b753fb04d94a1ea44c71ee
keineph <ephraim@posteo.de>
2017-11-20 23:18:35 +0100

    update

--- Ethik/mittels_monitor_md_zu_slide_konvertieren.sh
+++ Ethik/mittels_monitor_md_zu_slide_konvertieren.sh
@@ -4,7 +4,7 @@
 inotifywait -mq -e modify --format %w%f /home/effe/Git/Gitbooks/prasentationen/Ethik/ | while read FILE
 do
 	echo "Als Slide erstellt: $(basename $FILE .md)"
-	cat /home/effe/HTML/keineph-slides/reveal-slides/vorlagen/reveal1.txt $FILE /home/effe/HTML/keineph-slides/reveal-slides/vorlagen/reveal2.txt > /home/effe/HTML/keineph-slides/ethik-slides/$(basename $FILE .md).html
+	cat /home/effe/Git/dorkeinath.github.io/slides_html/reveal-slides/vorlagen/reveal1.txt $FILE /home/effe/Git/dorkeinath.github.io/slides_html/reveal-slides/vorlagen/reveal2.txt > /home/effe/Git/dorkeinath.github.io/slides_html/ethik-slides/$(basename $FILE .md).html
 
 done
 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Präsentationen
 =======
 
-Präsentationen, die ich als <a href="http://xcosx.de/mgb/slides.php">Slides</a> im Unterricht verwendet habe.
+Präsentationen, die ich als <a href="https://dorkeinath.github.io/">Slides</a> im Unterricht verwendet habe.
 
 Die Präsentationen im Fach Ethik können von SchülerInnen für ihre Klausurvorbereitung  oder Unterrichtsnachbereitung verwendet werden, quasi wie Tafelaufschriebe.
 
